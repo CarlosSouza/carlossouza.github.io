1d010bfa8938c18547d468d0e73b5a8ccb20e33d
Carlos Eduardo de Souza <carlos.souza@me.com>
2015-03-03 22:43:25 -0300

    jekyll

---
+++ _site/js/main.js
@@ -0,0 +1,34 @@
+/*
+  --------------
+  FUNCOES EXTERNAS
+  --------------
+*/
+
+var size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
+
+
+$(document).ready(function(){
+
+
+/*
+  --------------
+  DEFINICOES GERAIS
+  --------------
+*/
+
+  // Telas médias
+    /*
+    if (size.indexOf("medium") !=-1) {
+
+    }
+    */
+
+  // Telas Grandes
+    /*
+    if (size.indexOf("large") !=-1) {
+      
+    }
+    */
+
+
+});
